ca18d28f4fc378437de937be6262bb35e03edb13
RJ <rjnay1984@gmail.com>
2022-04-11 16:07:13 -0500

    Angular material (#3)
    * add angular material
    
    * navigation component
    
    * heroku frontend deploy

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './core/material/material.module';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigationComponent
       ],
     }).compileComponents();
   });
@@ -30,6 +34,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('frontend');
   });
 });
---
+++ frontend/src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  @Input() appTitle!: string;
+
+  constructor() { }
+
+}
