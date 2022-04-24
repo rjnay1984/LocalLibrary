38bb771ebb3b56ad2c565597e8910a9290d584be
RJ Nay <rjnay1984@gmail.com>
2022-04-24 13:56:52 -0500

    sign in form markup

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialModule } from '../core/material/material.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 
-
 @NgModule({
-  declarations: [
-    SignInComponent
-  ],
+  declarations: [SignInComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    MaterialModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- frontend/src/app/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
+  hide = true;
+  signInForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
   constructor() {}
 }
